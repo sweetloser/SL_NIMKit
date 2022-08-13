fa708c222fa72b7c7e1c25316f3fbdfe67117c01
sweetloser <18272160172@163.com>
2022-08-13 13:49:37 +0800

    init

---
+++ Example/DemoApplication/CustomChatCell/Message/NTESContentView.h
@@ -0,0 +1,18 @@
+//
+//  NTESContentView.h
+//  DemoApplication
+//
+//  Created by chris on 15/11/1.
+//  Copyright © 2015年 chris. All rights reserved.
+//
+
+#import "NIMKit.h"
+#import "NIMSessionMessageContentView.h"
+
+@interface NTESContentView : NIMSessionMessageContentView
+
+@property (nonatomic,strong) UILabel *titleLabel;
+
+@property (nonatomic,strong) UILabel *subTitleLabel;
+
+@end
---
+++ Example/DemoApplication/NTESAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  NTESAppDelegate.h
+//  DemoApplication
+//
+//  Created by chris on 15/10/7.
+//  Copyright © 2015年 chris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NTESAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
